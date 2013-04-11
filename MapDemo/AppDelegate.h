7158b3dade9eb88b031d017f673200fcf27e5412
marujun <marujunyy@qq.com>
2013-04-11 16:11:02 +0800

    Initial Commit

---
+++ MapDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MapDemo
+//
+//  Created by marujun on 13-4-11.
+//  Copyright (c) 2013年 马汝军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MapDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MapDemo
+//
+//  Created by marujun on 13-4-11.
+//  Copyright (c) 2013年 马汝军. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
